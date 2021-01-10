19d13da5c6ae999fb7280edaed8d6c9a6de6ab85
Yoshiaki Kanazawa <k.yoshiaki1226@gmail.com>
2021-01-10 15:22:23 +0900

    認証確認をアノテーションで実装

--- listing_webiner/lws/views/webiner.py
+++ listing_webiner/lws/views/webiner.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from lws.models.webiner_lists import WebinerLists
 
+@login_required
 def webiner_list(request):
-    if request.user.is_authenticated:
-        webiners = WebinerLists.objects.all().order_by('date')
-        return render(request, 'lws/webiner_list.html', {'webiners': webiners})
-    else:
-        return redirect(to='/users/login')
+    webiners = WebinerLists.objects.all().order_by('date')
+    return render(request, 'lws/webiner_list.html', {'webiners': webiners})
