04e96c44e53975588110066675570dd6901dcc2f
Yoshiaki Kanazawa <k.yoshiaki1226@gmail.com>
2021-01-16 10:24:31 +0900

    定義変更

--- listing_webiner/lws/urls.py
+++ listing_webiner/lws/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from lws.views import webiner
+from lws.views import webiner_view
 
 app_name = 'lws'
 urlpatterns = [
-    path('webiner/', webiner.webiner_list, name='webiner_list'),
+    path('webiner/', webiner_view.webiner_list, name='webiner_list'),
+    path('webiner/register', webiner_view.register_webiner, name='register_webiner'),
 ]
